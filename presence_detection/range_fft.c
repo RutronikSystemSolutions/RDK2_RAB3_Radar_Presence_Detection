ea846253476e66e397395ebb7f6934209a9eb81a
Jordan-Rose <jordan.rose@picosens.de>
2024-07-12 15:22:45 +0200

    Add Rutronik's disclaimer everywhere

--- presence_detection/range_fft.c
+++ presence_detection/range_fft.c
@@ -3,13 +3,17 @@
  *
  *  Created on: 25 Jun 2024
  *      Author: jorda
+ *
+ * Rutronik Elektronische Bauelemente GmbH Disclaimer: The evaluation board
+ * including the software is for testing purposes only and,
+ * because it has limited functions and limited resilience, is not suitable
+ * for permanent use under real conditions. If the evaluation board is
+ * nevertheless used under real conditions, this is done at one’s responsibility;
+ * any liability of Rutronik is insofar excluded
  */
 
 
 #include "range_fft.h"
-
-// TODO remove me
-#include <stdio.h>
 
 /**
  * @brief Perform range FFT on the samples contained inside the frame buffer
